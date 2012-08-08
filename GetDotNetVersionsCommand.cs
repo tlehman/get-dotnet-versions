99b8df0a14d65440bec35a222d930995cc59e648
Tobi Lehman <tobi.lehman@gmail.com>
2012-08-07 21:52:55 -0700

    Initial Commit

---
+++ GetDotNetVersionsCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+
+namespace GetDotNetVersions
+{
+    [Cmdlet("Get", "DotNetVersions")]
+    public class GetDotNetVersionsCommand : Cmdlet
+    {
+
+    }
+}
