2ab55c9103133be3ed3ad0fd72ecd3c64b3d95f9
Tobi Lehman <tobi.lehman@gmail.com>
2012-08-07 22:14:29 -0700

    Fill in cmdlet boilerplate

--- GetDotNetVersionsCommand.cs
+++ GetDotNetVersionsCommand.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Management;
+using System.Management.Automation;
+using Microsoft.Win32;
 
 namespace GetDotNetVersions
 {
     [Cmdlet("Get", "DotNetVersions")]
     public class GetDotNetVersionsCommand : Cmdlet
     {
-
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+            // HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP
+            RegistryKey rk = Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Microsoft").OpenSubKey("NET Framework Setup").OpenSubKey("NDP");
+            WriteObject(rk.GetSubKeyNames());
+        }
     }
 }
